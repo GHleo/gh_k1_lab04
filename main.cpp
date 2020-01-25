43d18da96e2be81cf376a2b3858a1adcbf5504be
GHLeo <ggleonid@gmail.com>
2020-01-25 18:39:28 +0500

    250120

--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+//11. 3sin(x) + x4 * [1 + (x–2/x) / (x+2/x)] * sin (x) -1
+//Величины XN, XK и DX
 
-int main()
-{
-    int age;
-    double weight;
-    char ch ='y';
-    while(ch == 'y' or ch == 'Y') {
-        std::cout << "Input age: ";
-        std::cin >> age;
-        std::cout << "Input weight: ";
-        std::cin >> weight;
-        std::cout << "Your age: " << age << "\t your weight: " << weight << std::endl;
+// расчетная функция
+double f_11(double x){
+    return 3*sin(x) + x*4 * abs(1 + (x - 2/x) / (x + 2/x)) * sin(x) - 1;
+}
 
-        std::cout << "Продолжить работу? (y/n) ";
-        std::cin >> ch;
-        if(ch == 'n' or ch == 'N'){
-            break;
-        }
+int main() {
+    double x, XN, XK, DX; //x -аргумент функции; XN, XK - начальное и конечное значение для аргумента соответсвенно; DX - шаг
+    cout << "Введите начальное значение для аргумента: ";
+    cin >> XN;
+    cout << "Введите конечное значение для аргумента: ";
+    cin >> XK;
+    cout << "Введите шаг: ";
+    cin >> DX;
+    for (double x = XN; x <= XK; x += DX) {
+        cout << fixed;//количеству знаков после запятой перед precision
+        cout.precision(6);
+        cout << f_11(x) << endl;
     }
     return 0;
-}
+    }
+
+
+/*    double a, b, h;
+    cin >> a >> b>> h;
+    for (double x = a; x <= b; x += h) {
+        cout << fixed << setprecision(3) <<x<< " " << 3 * sin(x) << endl;
+    }
+    return 0;*/
+
+
