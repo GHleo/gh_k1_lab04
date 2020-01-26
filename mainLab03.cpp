edc1e1e339ca081903e10f6e93bddb9fe7af1d4d
GHLeo <ggleonid@gmail.com>
2020-01-26 13:33:55 +0500

    260120 obmen

--- mainLab03.cpp
+++ mainLab03.cpp
@@ -1,4 +1,42 @@
-//
-// Created by iLeoMac on 2020-01-26.
-//
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+//x - cos(x) = 0
+//Величины XN, XK и DX
+
+// расчетная функция
+double f_2(double x){
+    return x - cos(x);
+}
+int main()
+{
+    double a,b,eps,x,dx;
+    do
+    {
+        cout<<"Enter diapazone[a;b] of iterations\r\n";
+        cout<<"a = ";cin>>a;
+        cout<<"b = ";cin>>b;
+        cout<<"Enter accuracy e ";cin>>eps;
+        //Шаг определяем по точности вычислений
+        //Погрешность f(x[i]) - f(x[i - 1]) < x[i] - x[i - 1] = h
+        dx = (b - a)*(eps/10);//Мин шаг точность/10
+        x = a;
+        //Вычисления останавливаем когда добиваемся
+        //требуемой точности вычислений eps < fabs(f(x))
+        //или же достигаем конца промежутка
+        while(eps < fabs(f(x)) && x <= b)
+            x += dx;
+        if(b < x)
+            cout<<"[a;b] isn't contain roots\r\n";
+        else
+        {
+            cout<<"root      : "<<x<<"\r\n";
+            cout<<"acuracy e : "<<fabs(f(x))<<"\r\n";
+        }
+        cout<<"Press Y for new input\r\n";
+    }
+    while(toupper(getch()) == 'Y');
+    return 0;
+}
 
