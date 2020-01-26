b17d7337a3ea929ce1b2d21256b80b821fb7be29
nikeini <60290301+nikeini@users.noreply.github.com>
2020-01-26 11:34:47 +0300

    Update obmen.cpp

--- obmen.cpp
+++ obmen.cpp
@@ -1,4 +1,27 @@
-//
-// 1
-//
+#include<cmath>
+#include <iostream>
+using namespace std;
 
+const double epsilon = 0.00000001;
+	double f(double x)
+	{
+		return x-cos(x);
+	}
+
+	int main()
+	{
+		double a, b, c;
+		int k;
+		cin >> a >> b;
+		k = 0;
+		while (b - a > epsilon) {
+			k += 1;
+			c = (a + b) / 2;
+			if (f(b) * f(c) < 0)
+				a = c;
+			else
+				b = c;
+		}
+		cout << (a + b) / 2 << endl;
+		return 0;
+	}
