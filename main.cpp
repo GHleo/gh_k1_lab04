bbab3014afccb43980896c50661b35366e4e05e3
GHLeo <ggleonid@gmail.com>
2020-01-26 12:39:57 +0500

    260120 лаба готова

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ double f_24(double x){
 
 int main() {
     double x, XN, XK, DX; //x -аргумент функции; XN, XK - начальное и конечное значение для аргумента соответсвенно; DX - шаг
-    double a,b; //концы отрезка
+    double a,b,eps; //концы отрезка
     cout << "Введите начальное значение для аргумента: ";
     cin >> XN;
     cout << "Введите конечное значение для аргумента: ";
@@ -27,19 +27,43 @@ int main() {
        cout.width(18);// cout.width - member function
        cout << setprecision(10) << f_24(x) << endl;// << setw(18) - задает ширину столбца для значения
     }
-
+//Нахождение экстремумов функции
     cout << "Вводим границу поиска [a, b]" << endl;
-    cin >> a >> b;
-    double min = f_24(a);
-    double eps = fabs(a - b) / 100; //
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    cout<<"Введите точность e ";cin>>eps;
+    //Шаг определяем по точности вычислений
+    //Погрешность f(x[i]) - f(x[i - 1]) < x[i] - x[i - 1] = h
+    DX = (b - a)*(eps/10);//Мин шаг точность/10
 
+
+    double min = f_24(a);
+    //eps = fabs(a - b) / 100; //делим отрезок на 100 частей
     for (a += eps; a <= b; a += eps)
     {
         if (f_24(a) < min)
             min = f_24(a);
     }
+    cout << "min = " << min << endl;
+    //Корни
+    //Вычисления останавливаем когда добиваемся
+    //требуемой точности вычислений eps < fabs(f(x))
+    //или же достигаем конца промежутка
+    x = a;
+    while(eps < fabs(f_24(x)) && x <= b)
+        x += DX;
+    if(b < x)
+        cout<<"[a;b] Корней нет\r\n";
+    else
+    {
+        cout<<"Корень      : "<<x<<"\r\n";
+        cout<<"Точность e : "<<fabs(f_24(x))<<"\r\n";
+    }
+    cout<<"Нажмите Y для нового ввода\r\n";
 
-    cout << "min = " << min;
+    while(toupper(getchar()) == 'Y');
     return 0;
     }
 
