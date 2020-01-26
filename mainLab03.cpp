bb1cd9de0ce5be2087bf9a24c0fb004ecd6637c8
GHLeo <ggleonid@gmail.com>
2020-01-26 20:18:39 +0500

    260120 obmen

--- mainLab03.cpp
+++ mainLab03.cpp
@@ -16,17 +16,18 @@ typedef double(*function)(double x); // задание типа function
 
 double solve(function f_3, function dfx, double x0) {
     double x1 = x0 - f_3(x0) / dfx(x0); // первое приближение
-    int k;
     while (fabs(x1 - x0) > eps) { // пока не достигнута точность 0.000001
         k++;
         x0 = x1;
         x1 = x0 - f_3(x0) / dfx(x0); // последующие приближения
     }
+    return x;
 }
 int main() {
     i=0;
-    int iter;
-    double root;
+    cout << "Метод Ньютона. Корень = " << solve(f_3, dfx, 1)<< endl;
+    cout << "Метод Ньютона. Количество итераций = " << k << endl;
+
     cout << "Введите начальное значение для аргумента: ";
     cin >> a;
     cout << "Введите конечное значение для аргумента: ";
@@ -41,25 +42,6 @@ int main() {
                 exit(0);
             }
         }
-
-
-   // printf("%f\n", solve(f_3, dfx, 1)); // вывод на экран
-    cout << "Метод Ньютона. Корень = " << solve(f_3, dfx, 1)<< endl;
-    cout << "Метод Ньютона. Количество итераций = " << k << endl;
-
-/*    x = a;
-    while(eps < fabs(f_3(x)) && x <= b)
-        cout << fabs(f_3(x));
-        x += 0.1;
-    if(b < x)
-        cout<<"[a;b] Корней нет\r\n";
-    else
-    {
-        cout<<"Корень      : "<<x<<"\r\n";
-        cout<<"Точность e : "<<fabs(f_3(x))<<"\r\n";
-    }
-    cout<<"Нажмите Y для нового ввода\r\n";*/
-
 
     return 0;
 }
