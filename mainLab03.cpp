6f31135a186a4ae4999f24b80fe0e7c41c5501df
GHLeo <ggleonid@gmail.com>
2020-01-26 16:30:55 +0500

    260120 obmen

--- mainLab03.cpp
+++ mainLab03.cpp
@@ -12,27 +12,15 @@ double f_3(double x){
     return x - cos(x);
 }
 int main() {
-
-   // Простая итерация    Хj+1 = Xj - f(Xj).
-/*    int n=0;
-    x=-5;
-//double x=-2.0,y,b,eps=0.000001;
-    do {
-        y=f_3(x);
-        b=fabs(x-y);
-        x=y;
-        n++;
-    }while (b>=eps && n<100);
-    cout<<"Root x ="<<x<<"\n";
-    cout<<"Iterations n= "<<n<<"\n";
-    getchar();
-    return 0;*/
-
-        for ( x= 0.7; x <= 1; x += eps) {
+    cout << "Введите начальное значение для аргумента: ";
+    cin >> a;
+    cout << "Введите конечное значение для аргумента: ";
+    cin >> b;
+        for ( x= a; x <= b; x += eps) {
             y = f_3(x);
             //cout << "y = " << y << endl;
             if (fabs(y) <= eps) {
-                cout << "x = " << x << endl;
+                cout << "Метод простых итераций. Корень = " << x << endl;
                 getchar();
                 exit(0);
             }
