53ade2aed8fb7f9581216f411d81f9e54c4229e5
GHLeo <ggleonid@gmail.com>
2020-01-26 16:33:45 +0500

    260120 obmen

--- mainLab03.cpp
+++ mainLab03.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //Величины XN, XK и DX
 
 const double eps = 0.00000001; // погрешность
-double x,y,a,b;
+double x,y,a,b,i;
 // расчетная функция
 double f_3(double x){
     return x - cos(x);
@@ -17,10 +17,11 @@ int main() {
     cout << "Введите конечное значение для аргумента: ";
     cin >> b;
         for ( x= a; x <= b; x += eps) {
+            i += 1;
             y = f_3(x);
-            //cout << "y = " << y << endl;
             if (fabs(y) <= eps) {
                 cout << "Метод простых итераций. Корень = " << x << endl;
+                cout << "Метод простых итераций. Количество итераций = " << i << endl;
                 getchar();
                 exit(0);
             }
