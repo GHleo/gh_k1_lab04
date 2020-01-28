53d4411d3948e1fdca3ed644e7cdaeb35e83f5fd
GHleo <ggleonid@gmail.com>
2020-01-28 14:58:40 +0500

    Update mainLab03.cpp

--- mainLab03.cpp
+++ mainLab03.cpp
@@ -14,7 +14,7 @@ double f_3(double x){return x - cos(x);}
 double dfx(double x) { return 1+sin(x); } // производная функции
 typedef double(*function)(double x); // задание типа function
 
-double solve(function f_3, function dfx, double x0) {
+double metN(function f_3, function dfx, double x0) {
     double x1 = x0 - f_3(x0) / dfx(x0); // первое приближение
     while (fabs(x1 - x0) > eps) { // пока не достигнута точность 0.000001
         k++;
@@ -23,26 +23,48 @@ double solve(function f_3, function dfx, double x0) {
     }
     return x1;
 }
+double metSI(double beginV, double endV) {
+	for (x = beginV; x <= endV; x += eps) {
+		i += 1;
+		y = f_3(x);
+		if (fabs(y) <= eps) {
+			cout << "ITER Метод простых итераций. Корень = " << x << endl;
+			cout << "ITER Метод простых итераций. Количество итераций = " << i << endl;
+			getchar();
+			exit(0);
+		}
+	}
+}
 int main() {
+	setlocale(LC_ALL, "Russian");
+	double XN;
+	double XK = 0.8;
     i=0;
-    cout << "Метод Ньютона. Корень = " << solve(f_3, dfx, 1)<< endl;
-    cout << "Метод Ньютона. Количество итераций = " << k << endl;
+
 
     cout << "Введите начальное значение для аргумента: ";
     cin >> a;
     cout << "Введите конечное значение для аргумента: ";
-    cin >> b;
-        for ( x= a; x <= b; x += eps) {
-            i += 1;
-            y = f_3(x);
-            if (fabs(y) <= eps) {
-                cout << "Метод простых итераций. Корень = " << x << endl;
-                cout << "Метод простых итераций. Количество итераций = " << i << endl;
-                getchar();
-                exit(0);
-            }
-        }
+	cin >> b;
+	XN = a;
+	metSI(XN, XK);
+	cout << "N Метод Ньютона. Корень = " << metN(f_3, dfx, 1) << endl;
+	cout << "N Метод Ньютона. Количество итераций = " << k << endl;
+
+
+			double a, b, c, e;
+			a = 0;
+			cin >> b;
+			cin >> e;
+			while (b - a > e) {
+				c = (a + b) / 2;
+				if (f(b) * f(c) < 0)
+					a = c;
+				else
+					b = c;
+			}
+			cout << (a + b) / 2 << endl;
+
 
     return 0;
 }
-
