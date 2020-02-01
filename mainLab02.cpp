a2a28f210e4d6e0aa652d90eef6a5b2b30c3cd0a
GHLeo <ggleonid@gmail.com>
2020-02-01 19:04:06 +0500

    01022020

--- mainLab02.cpp
+++ mainLab02.cpp
@@ -29,27 +29,18 @@ int main() {
     float s, y, t, k, d;
     // s - сумма Тейлора; d- факториал; k - значение предыдущего члена
     double x;
-    int i = 0, n;
-    n = 1;
+    int  n;
+
     for (x = 0.0; x <= 1.0; x += 0.2) {
-        //n = 0; //порядковый номер в формуле
+        n = 1; //порядковый номер в формуле
         s = x;
-        t = 1.0;
+        t = 1;
 
-        while (abs(t) >= 0.000001) {
-
-            d = fact(2 * n);
-            cout << "factorial d = " << d << endl;
-            //if (n != 0)
-             //   k = pow(-1, n) * ((2 * pow(n, 2) + 1)/d) * (pow(x, 2 * n));
-                    //((pow(-1, n - 1) * (2 * pow(n - 1, 2) + 1) * (pow(x, 2 * (n - 1)))) / d);
-
-           // else
-           //   k = 1;
-            t = pow(-1, n) * ((2 * pow(n, 2) + 1)/d) * (pow(x, 2 * n));//ряд Тейлора
+        while (abs(t) > 0.000001) {
+            t = pow(-1, n) * ((2 * pow(n, 2) + 1)/fact(2*n)) * (pow(x, 2 * n));//ряд Тейлора
             s += t;
-
-            n++;
+            n += 1;
+            cout.precision(8);
             cout << "в цикле while смотим  n = "<< n << " и t = " << t << " и s =" << s <<endl;
         }
 
@@ -72,9 +63,6 @@ int main() {
         cout.precision(0);
 
         cout << n << endl;
-
-        i++;
-
 
     }
 
