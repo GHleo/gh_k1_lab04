4c703e39d3799fe717ee80036c5b170bc12488df
GHLeo <ggleonid@gmail.com>
2020-02-01 19:19:05 +0500

    01022020

--- mainLab02.cpp
+++ mainLab02.cpp
@@ -26,22 +26,24 @@ int main() {
 
     cout << "x" << "  " << "Y(x)" << " " << "S(x)" << "  "  << "n" << endl;
 
-    float s, y, t, k, d;
+    //float s, y,  k, d;
     // s - сумма Тейлора; d- факториал; k - значение предыдущего члена
-    double x;
+    long double x,t,s,y,eps=0.000001;
     int  n;
 
     for (x = 0.0; x <= 1.0; x += 0.2) {
         n = 1; //порядковый номер в формуле
         s = x;
-        t = 1;
+        t = 1.0;
 
-        while (abs(t) > 0.000001) {
+        while (fabs(t) >= eps) {
+            cout << "в цикле while fabs(t) = "<< fabs(t) << " x = " << x <<endl;
             t = pow(-1, n) * ((2 * pow(n, 2) + 1)/fact(2*n)) * (pow(x, 2 * n));//ряд Тейлора
             s += t;
-            n += 1;
+
             cout.precision(8);
             cout << "в цикле while смотим  n = "<< n << " и t = " << t << " и s =" << s <<endl;
+            n += 1;
         }
 
         y = 1 - (pow(x, 2) / 2) * cos(x) - (x / 2) * sin(x);//вычисление функции
