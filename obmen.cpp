93adc82649b6e2cb4663f05c4bd50c9277f23e9b
nikeini <60290301+nikeini@users.noreply.github.com>
2020-02-01 14:36:12 +0300

    Update obmen.cpp

--- obmen.cpp
+++ obmen.cpp
@@ -28,3 +28,98 @@ int main() {
 	
 
 }
+
+
+
+
+#include <iostream>
+#include <iomanip> 
+#include<cmath>
+#include <ctime>
+using namespace std;
+
+
+int fact(int n) {
+
+	if (n < 0)
+
+		return 0;
+
+	if (n == 0)
+
+		return 1;
+
+	else
+
+		return n * fact(n - 1);
+
+}
+
+
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "x" << " ";
+
+	cout << "Y(x)" << " " << "S(x)" << " ";
+
+	cout << "n" << endl;
+
+	float s, y, t, k, d;
+
+	double x;
+
+	int i = 0, w;
+
+	for (x = 0.0; x <= 1.0; x += 0.2) {
+
+		w = 0; s = 0; k = 1.0;
+
+		while (k >= 0.000001) {
+
+			d = fact(2 * w );
+
+			if (w != 0)
+				
+				 k = (((pow(-1, w ) * (2 * pow(w, 2) + 1) * (pow(x, 2 * w)))) / d) / ((pow(-1, w - 1) * (2 * pow(w-1, 2) + 1) * (pow(x, 2 * (w-1)))) / d);
+
+			else
+
+				k = 1;
+
+			t = ((pow(-1, w ) * (2 * pow(w, 2) + 1) * (pow(x, 2 * w))) / d);
+
+			s += t;
+
+			w++;
+
+		}
+
+		y = (1-pow(x,2)/2)*cos(x)-(x/2)*sin(x);
+
+		cout.setf(ios::fixed);
+
+		cout.precision(1);
+
+		cout << x << " ";
+
+		cout.setf(ios::fixed);
+
+		cout.precision(6);
+
+		cout << y << " " << s << " ";
+
+		cout.setf(ios::fixed);
+
+		cout.precision(0);
+
+		cout << w << endl;
+
+		i++;
+
+	}
