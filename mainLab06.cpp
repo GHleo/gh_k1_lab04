a5b57b7665d947ff86d2c1f0db2f5b965caeb1a2
GHLeo <ggleonid@gmail.com>
2020-02-02 14:23:31 +0500

    01022020

--- mainLab06.cpp
+++ mainLab06.cpp
@@ -18,42 +18,42 @@ struct I_print{	//данные для печати результатов инт
 };
 
 // Функция для печати таблицы результатов (из методички)
-void PrintTabl(I_print i_prn[],int k)
+void PrintTabl(I_print i_prn[], int k)
 {
-    const int m=4;//число столбцов таблицы
-    int wn[m]={12,18,18,18};//ширина столбцов таблицы
-    string title[m]={"Function","Integral","IntSum","N "};
+    const int m = 4;
+    int wn[m] = { 12,18,18,10 };
+    string title[m] = { "Function" ,"Integral" ,"IntSum" ,"N " };
     int size[m];
-    for(int i=0;i<m;i++)
-        size[i]=title[i].length();
-//шапка таблицы
-    cout<<char(124)<<setfill(char(61));
-    for(int j=0;j<m-1;j++)
-        cout<<setw(wn[j])<<char(124);
-    cout<<setw(wn[m-1])<<char(124)<<endl;
+    for (int i = 0; i < m; i++)
+        size[i] = title[i].length();
 
-    cout<<char(124);
-    for(int j=0;j<m;j++)
-        cout<<setw((wn[j]-size[j])/2)<<setfill(' ')<<' '<<title[j]
-            <<setw((wn[j]-size[j])/2)<<char(124);
-    cout<<endl;
-    for(int i=0;i<k;i++)
-    {//заполнение таблицы
-        cout<<char(124)<<fixed;
-        for(int j=0;j<m-1;j++)
-            cout<<setfill(char(126))<<setw(wn[j])<<char(124);
-        cout<<setw(wn[m-1])<<char(124)<<setfill(' ')<<endl;
+    cout << char(218) << setfill(char(196));
+    for (int j = 0; j < m - 1; j++)
+        cout << setw(wn[j]) << char(194);
+    cout << setw(wn[m - 1]) << char(191) << endl;
+    cout << char(179);
+    for (int j = 0; j < m; j++)
+        cout << setw((wn[j] - size[j]) / 2) << setfill(' ') << ' ' << title[j]
+             << setw((wn[j] - size[j]) / 2) << char(179);
+    cout << endl;
+    for (int i = 0; i < k; i++)
+    {
+        cout << char(195) << fixed;
+        for (int j = 0; j < m - 1; j++)
+            cout << setfill(char(196)) << setw(wn[j]) << char(197);
+        cout << setw(wn[m - 1]) << char(180) << setfill(' ') << endl;
+        cout << char(179) << setw((wn[0] - (i_prn[i].name).length()) / 2) << ' ' << i_prn[i].name
+             << setw((wn[0] - (i_prn[i].name).length()) / 2) << char(179);
 
-        cout<<char(124)<<setw((wn[0]-(i_prn[i].name).length()) /2)<<' '<<i_prn[i].name
-            <<setw((wn[0]-(i_prn[i].name).length()) /2)<<char(124);
-        cout<<setw(wn[1]-1)<<setprecision(10)<<i_prn[i].i_toch<<char(124)
-            <<setw(wn[2]-1)<<i_prn[i].i_sum<<setprecision(6)<<char(124)
-            <<setw(wn[3]-1)<<i_prn[i].n<<char(124)<<endl;
+        cout << setw(wn[1] - 1) << setprecision(10) << i_prn[i].i_toch << char(179)
+             << setw(wn[2] - 1) << i_prn[i].i_sum << setprecision(6) << char(179)
+             << setw(wn[3] - 1) << i_prn[i].n << char(179) << endl;
     }
-    cout<<char(126)<<setfill(char(126));
-    for(int j=0;j<m-1;j++)
-        cout<<setw(wn[j])<<char(126);
-    cout<<setw(wn[m-1])<<char(126)<<setfill(' ')<<endl;
+
+    cout << char(192) << setfill(char(196));
+    for (int j = 0; j < m - 1; j++)
+        cout << setw(wn[j]) << char(193);
+    cout << setw(wn[m - 1]) << char(217) << setfill(' ') << endl;
 }
 
 // Функция 1 (f1 = x)
