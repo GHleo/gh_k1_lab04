99e8e5cb51b9bc3ec1a923b4c9a31c0b8f8e8367
nikeini <60290301+nikeini@users.noreply.github.com>
2020-02-15 10:32:08 +0300

    Update file_input_output.cpp

--- file_input_output.cpp
+++ file_input_output.cpp
@@ -1,4 +1,29 @@
-//
-// Created by iLeoMac on 2020-02-15.
-//
+#include<fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch;
+	int p, n;
+	int ar[100];
+	ifstream cod;
+	ofstream decod;
+	cod.open("C:/Users/den/source/repos/ConsoleApplication12/cod");
+	while (cod.get(ch))
+	{
+		p = int(ch);
+		ar[p] = p;
+
+
+	}
+	decod.open("C:/Users/den/source/repos/ConsoleApplication12/decod");
+	for (int i = 0; i < 100; i++)
+	{
+		if (ar[p] > 0)
+			decod << ar[p];
+	}
+
+	return 0;
+}
 
