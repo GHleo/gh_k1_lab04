d97c7c55fdcf2ccdb7159ba41120e5d8b89ad409
nikeini <60290301+nikeini@users.noreply.github.com>
2020-02-15 12:52:18 +0300

    Update file_input_output.cpp

--- file_input_output.cpp
+++ file_input_output.cpp
@@ -1,3 +1,4 @@
+#include<string>
 #include<fstream>
 #include <iostream>
 using namespace std;
@@ -5,25 +6,43 @@ using namespace std;
 int main()
 {
 	char ch;
-	int p, n;
-	int ar[100];
-    char S1[256];
-    ifstream cod; // for reading file
-	ofstream decod; // for writing file
-	cod.open("../cod.txt");
-    decod.open("../decod.txt");
-	while (!cod.eof())
-	{
-        cod.get(ch);
-        p = int(ch);
-        decod << p << endl;
-        cin >> S1;
-       // decod << ch << " - " << p << endl;
-       cout << ch;
-	}
-    std::cout << endl << "Текст для шифрования: " << endl;
-    cod.close();
+	int p = 0;
+	int mas[256];
+	for (int i = 0; i < 256; i++)
+		mas[i] = 0;
+
+
+	;
+	ifstream cod;
+	ofstream decod2;
+	fstream decod;
+	
+	decod2.open("C:/Users/den/source/repos/ConsoleApplication12/decod2.txt");
+	cod.open("C:/Users/den/source/repos/ConsoleApplication12/cod.txt");
+	decod.open("C:/Users/den/source/repos/ConsoleApplication12/decod.txt",fstream::in | fstream::out);
+		while (cod.get(ch))
+		{
+			cout << ch << endl;
+			p = int(ch);
+			mas[p] = p;
+			decod << p << endl;
+			
+			
+
+		}
+	
+	cod.close();
     decod.close();
+	for (int i = 0; i < 256; i++)
+
+	{
+		if (mas[i] > 0)
+		{
+			decod2 << char(mas[i]);
+		}
+ 	}
 	return 0;
 }
+
+
 
