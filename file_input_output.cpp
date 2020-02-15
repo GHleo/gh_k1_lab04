d5c43eee4a9e35cbd54cfaab2c635431f0d8f417
GHLeo <ggleonid@gmail.com>
2020-02-15 13:20:03 +0500

    150220

--- file_input_output.cpp
+++ file_input_output.cpp
@@ -7,23 +7,23 @@ int main()
 	char ch;
 	int p, n;
 	int ar[100];
-	ifstream cod;
-	ofstream decod;
-	cod.open("C:/Users/den/source/repos/ConsoleApplication12/cod");
-	while (cod.get(ch))
+    char S1[256];
+    ifstream cod; // for reading file
+	ofstream decod; // for writing file
+	cod.open("../cod.txt");
+    decod.open("../decod.txt");
+	while (!cod.eof())
 	{
-		p = int(ch);
-		ar[p] = p;
-
-
+        cod.get(ch);
+        p = int(ch);
+        decod << p << endl;
+        cin >> S1;
+       // decod << ch << " - " << p << endl;
+       cout << ch;
 	}
-	decod.open("C:/Users/den/source/repos/ConsoleApplication12/decod");
-	for (int i = 0; i < 100; i++)
-	{
-		if (ar[p] > 0)
-			decod << ar[p];
-	}
-
+    std::cout << endl << "Текст для шифрования: " << endl;
+    cod.close();
+    decod.close();
 	return 0;
 }
 
